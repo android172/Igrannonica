96c39164a6f0a9ab89a6d3bcba600fa74ea321de
Teodora Jeremic <tekiiicaaa99@gmail.com>
2022-03-08 22:29:39 +0100

    #20

--- src/frontend/Angular/src/app/app.component.ts
+++ src/frontend/Angular/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular';
   proba(){
-    //document.body.style.background = '#fff'
+   
   }
 }
 
--- src/frontend/Angular/src/app/app.module.ts
+++ src/frontend/Angular/src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { PocetnaStranaComponent } from './pocetna-strana/pocetna-strana.component';
 import { PrijavaComponent } from './prijava/prijava.component';
 import { RegistracijaComponent } from './registracija/registracija.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { RegistracijaComponent } from './registracija/registracija.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
