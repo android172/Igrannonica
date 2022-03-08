f7eb20eee7c9d6f8ea2c964545e8c7a1466d5572
dragana.andjelkovic <andjelkovic.draganaa@gmail.com>
2022-03-08 22:10:40 +0100

    #25 Konekcija sa bazom

---
+++ src/backend/dotNet/dotNet/DBKonekcija.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Aplikacija
+{
+    public class DBKonekcija
+    {
+        MySqlConnection connect = null;
+        static string server = "localhost";
+        static string database = "bazasql";
+        static string username = "root";
+        static string password = "";
+        static string port = "3308";
+        static string connectionString = "datasource="+server+";user="+username+";database="+database+";port="+port+";password="+password;  
+
+        public DBKonekcija()
+        {
+            connect = new MySqlConnection(connectionString); 
+            connect.Open(); 
+
+            if(connect.State == ConnectionState.Open)
+            {
+                Console.WriteLine("Connected to Database.");
+            }
+            else
+            {
+                Console.WriteLine("Not Connected to Database.");
+            }
+        }
+    }
+}
