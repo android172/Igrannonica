b14babf8701a78257531d84ccbfafc2c3ea99c52
Aleksandar Sindjelic <aco.sindjelic@gmail.com>
2022-03-09 17:00:19 +0100

    Instalacija node_modules i ispravka na backendu

--- src/backend/dotNet/dotNet/Controllers/AuthController.cs
+++ src/backend/dotNet/dotNet/Controllers/AuthController.cs
@@ -56,27 +56,18 @@ namespace dotNet.Controllers
         private Korisnik Authenticate(KorisnikDto korisnik)
         {
             Korisnik kor = db.dajKorisnika(korisnik.KorisnickoIme, korisnik.Sifra);
-            if (korisnik == null)
-                return null;
             return kor;
-            //Ovde se poziva baza i proverava da li korisnik postoji u njoj
-            if (korisnik.KorisnickoIme == "admin" && korisnik.Sifra == "admin") return new Korisnik(1, "admin", "admin", "admin", "admin@admin.com", "123456789");
-            return null;
         }
 
         [HttpPost("register")]
         public IActionResult Register(KorisnikRegister request) {
             //Pretraziti bazu da li korisnik postoji
             //Upisati ga u bazu ako ga nema
-            if (request.Sifra != request.Sifra2) return BadRequest("Losa sifra2");
-            if(db.dodajKorisnika(new Korisnik(0, request.KorisnickoIme, request.Ime, request.Sifra, request.Email, request.Telefon)))
+            if(db.dodajKorisnika(new Korisnik(0, request.KorisnickoIme, request.Ime, request.Sifra, request.Email,"")))
             {
                 return Ok("Registrovan korisnik");
             }
             return BadRequest("Vec postoji");
-
-            string odgovor = "Registrovan";
-            return Ok(new { odgovor }); ;
         }
         private void CreatePasswordHash(string password ,out byte[] passwordHash,out byte[] passwordSalt)
         {
