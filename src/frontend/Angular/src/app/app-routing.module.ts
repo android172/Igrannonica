e520cc2b281ae24103d05939e395b614dd0141cb
Ivana Dragovic <ivana.dragovic07@gmail.com>
2022-03-13 10:07:28 +0100

    #32 Dodat routing ka novim komponentama
    Dodate su nove putanje u app-routing.module.ts za komponente novi-eksperiment, kontakt i o-nama

--- src/frontend/Angular/src/app/app-routing.module.ts
+++ src/frontend/Angular/src/app/app-routing.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { KontaktComponent } from './kontakt/kontakt.component';
+import { NoviEksperimentComponent } from './novi-eksperiment/novi-eksperiment.component';
+import { ONamaComponent } from './o-nama/o-nama.component';
 import { PocetnaStranaComponent } from './pocetna-strana/pocetna-strana.component';
 import { PrijavaComponent } from './prijava/prijava.component';
 import { RegistracijaComponent } from './registracija/registracija.component';
 
 const routes: Routes = [
-  {path:"", redirectTo: "/pocetnastrana", pathMatch: "full"},
-  {path:'pocetnastrana', component:PocetnaStranaComponent},
+  {path:"", redirectTo: "/pocetna-strana", pathMatch: "full"},
+  {path:'pocetna-strana', component:PocetnaStranaComponent},
   {path:'prijava', component:PrijavaComponent},
-  {path:'registracija', component:RegistracijaComponent}
+  {path:'registracija', component:RegistracijaComponent},
+  {path:'novi-eksperiment', component:NoviEksperimentComponent},
+  {path:'kontakt', component:KontaktComponent},
+  {path:'o-nama', component:ONamaComponent}
 ];
 
 @NgModule({
