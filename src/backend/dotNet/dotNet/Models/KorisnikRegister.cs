56bed96417098b93fab7d71fe242313b4836fe51
dragana.andjelkovic <andjelkovic.draganaa@gmail.com>
2022-03-14 23:08:13 +0100

    #37 Klasa KorisnikRegister - FluentValidation

--- src/backend/dotNet/dotNet/Models/KorisnikRegister.cs
+++ src/backend/dotNet/dotNet/Models/KorisnikRegister.cs
@@ -1,10 +1,24 @@
-﻿namespace dotNet.Models
+﻿using FluentValidation;
+
+namespace dotNet.Models
 {
     public class KorisnikRegister
     {
+
         public string KorisnickoIme { get; set; }
         public string Ime { get; set; }
         public string Sifra { get; set; }
         public string Email { get; set; }
+    }
+
+    public class KorisnikRegistrationValidator : AbstractValidator<KorisnikRegister>
+    {
+        public KorisnikRegistrationValidator()
+        {
+            RuleFor(x => x.KorisnickoIme).NotEmpty().NotNull().MinimumLength(1).MaximumLength(30);
+            RuleFor(x => x.Ime).NotEmpty().NotNull().MinimumLength(1).MaximumLength(30);  
+            RuleFor(x => x.Sifra).NotEmpty().NotNull().MinimumLength(8).MaximumLength(60);
+            RuleFor(x => x.Email).NotEmpty().EmailAddress(); 
+        }
     }
 }
