469e105b68addec7690d948b78255befb23eb1bd
Aleksandar Sindjelic <aco.sindjelic@gmail.com>
2022-03-14 17:22:05 +0100

    #42 i #41 uradjena oba zadatka

---
+++ src/backend/dotNet/dotNet/Controllers/EksperimentController.cs
@@ -0,0 +1,47 @@
+﻿using dotNet.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EksperimentController : ControllerBase
+    {
+        private IConfiguration _config;
+        DBKonekcija db;
+        public EksperimentController(IConfiguration config)
+        {
+            _config = config;
+            db = new DBKonekcija(_config.GetConnectionString("connectionString"));
+        }
+
+        [HttpGet("Eksperimenti")]
+        public IActionResult experimenti(int id)
+        {
+            List<EksperimentDto> eksperimenti = db.eksperimenti(id);
+            if (eksperimenti.Count > 0)
+                return Ok(eksperimenti);
+
+            return BadRequest();
+        }
+
+
+        [HttpGet("Modeli")]
+        public IActionResult modeli(int id) {
+            List<ModelDto> modeli=db.modeli(id);
+            if (modeli.Count > 0)
+                return Ok(modeli);
+        return BadRequest("Nema modela"); 
+        }
+
+        [HttpGet("Podesavanja")]
+        public IActionResult podesavanja(int id) {
+            ANNSettings podesavanje = db.podesavanja(id);
+            if(podesavanja != null)
+                return Ok(podesavanje);
+            return BadRequest("Ne postoje podesavanja za ovaj model");
+        }
+
+    }
+}
--- src/backend/dotNet/dotNet/DBKonekcija.cs
+++ src/backend/dotNet/dotNet/DBKonekcija.cs
@@ -84,5 +84,95 @@ namespace dotNet
             connect.Close();
             return false; 
         }
+
+        public List<EksperimentDto> eksperimenti(int id)
+        {
+            connect.Open();
+            string query = "select * from eksperiment where vlasnik=@id";
+            MySqlCommand cmd = new MySqlCommand(query, connect);
+            cmd.Parameters.AddWithValue("@id", id);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<EksperimentDto> result = new List<EksperimentDto>();
+            while (reader.Read())
+            {
+                EksperimentDto ex = new EksperimentDto();
+                ex.Id = reader.GetInt32("id");
+                ex.Name = reader.GetString("Naziv");
+                result.Add(ex);
+            }
+            return result;
+        }
+        public List<ModelDto> modeli(int id)
+        {
+            connect.Open();
+            string query = "select * from model where ideksperimenta=@id";
+            MySqlCommand cmd = new MySqlCommand(query, connect);
+            cmd.Parameters.AddWithValue("@id", id);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            List<ModelDto> result = new List<ModelDto>();
+            while (reader.Read())
+            {
+                ModelDto ex = new ModelDto();
+                ex.Id = reader.GetInt32("id");
+                ex.Name = reader.GetString("Naziv");
+                ex.CreatedDate = reader.GetDateTime("napravljen");
+                ex.UpdatedDate = reader.GetDateTime("obnovljen");
+                result.Add(ex);
+            }
+            return result;
+        }
+        public ANNSettings podesavanja(int id)
+        {
+            connect.Open();
+            string query = "select * from podesavanja where id=@id";
+            MySqlCommand cmd = new MySqlCommand(query, connect);
+            cmd.Parameters.AddWithValue("@id", id);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                ProblemType fun;
+                if (reader.GetString("Problemtype").Equals("Reggresion")) fun = ProblemType.Regression;
+                else fun = ProblemType.Classification;
+                ANNSettings settings = new ANNSettings(fun, reader.GetFloat("LearningRate"), reader.GetInt32("BatchSize"),
+                    reader.GetInt32("numberOfEpochs"), reader.GetInt32("inputSize"), reader.GetInt32("OutputSize"), 
+                    HiddenLayers(reader.GetString("HiddenLayers")), aktivacionefunkcije(reader.GetString("aktivacionefunkcije")));
+
+                return settings;
+            }
+            return null;
+        }
+        private int[] HiddenLayers(string niz)
+        {
+            List<int> hiddenLayers = new List<int>();
+            foreach(string layer in niz.Split(','))
+            {
+                hiddenLayers.Add(int.Parse(layer));
+            }   
+            return hiddenLayers.ToArray();
+        }
+        private ActivationFunction[] aktivacionefunkcije(string niz)
+        {
+            List<ActivationFunction> funkcije = new List<ActivationFunction>();
+            foreach (string layer in niz.Split(','))
+            {
+                switch (layer)
+                {
+                    case "r" :
+                        funkcije.Add(ActivationFunction.ReLU);
+                        break;
+                    case "lr":
+                        funkcije.Add(ActivationFunction.LeakyReLU);
+                        break;
+                    case "s":
+                        funkcije.Add(ActivationFunction.Sigmoid);
+                        break;
+                    case "t":
+                        funkcije.Add(ActivationFunction.Tanh);
+                        break;
+                }
+            }
+            return funkcije.ToArray();
+        }
+
     }
 }
