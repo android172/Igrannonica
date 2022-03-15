2017994271974d973c3bad842935c60bf4811240
dragana.andjelkovic <andjelkovic.draganaa@gmail.com>
2022-03-15 03:15:06 +0100

    #37 Klasa KorisnikRegister prosirena novim funkcionalnostima

--- src/backend/dotNet/dotNet/Models/KorisnikRegister.cs
+++ src/backend/dotNet/dotNet/Models/KorisnikRegister.cs
@@ -15,10 +15,30 @@ namespace dotNet.Models
     {
         public KorisnikRegistrationValidator()
         {
-            RuleFor(x => x.KorisnickoIme).NotEmpty().NotNull().MinimumLength(1).MaximumLength(30);
+            RuleFor(x => x.KorisnickoIme).NotEmpty().NotNull().MinimumLength(1).MaximumLength(30)
+                .Must(y => ValidUsername(y)).WithMessage("Uneti su nedozvoljeni pocetni karakteri!");
+          
             RuleFor(x => x.Ime).NotEmpty().NotNull().MinimumLength(1).MaximumLength(30);  
             RuleFor(x => x.Sifra).NotEmpty().NotNull().MinimumLength(8).MaximumLength(60);
-            RuleFor(x => x.Email).NotEmpty().EmailAddress(); 
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().Must(ValidEmail);
+        }
+
+        protected bool ValidEmail(string email)
+        {
+            email = email.Replace(" ", "");
+            return true;
+        }
+        protected bool ValidUsername(string username)
+        {
+            username = username.Replace(" ", "");
+            username = username.ToLower();
+
+            // regularni izraz 
+            if (username[0].Equals('-') || username[0].Equals('_') || username[0].Equals('/') || username[0].Equals('\\') || username[0].Equals('+') || username[0].Equals('=') || username[0].Equals('?') || username[0].Equals('#'))
+            {
+                return false;
+            }
+            return true; 
         }
     }
 }
