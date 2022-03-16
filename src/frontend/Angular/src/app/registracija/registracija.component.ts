ef5d28d42dadbe78c806b3bc49de89175eb932de
Teodora Jeremic <tekiiicaaa99@gmail.com>
2022-03-16 19:24:01 +0100

    #36 Provera registracije korisnika
    - Proverava se da li je korisnik registrovan
    - Proverava se validnost korisnickog imena i email-a

--- src/frontend/Angular/src/app/registracija/registracija.component.ts
+++ src/frontend/Angular/src/app/registracija/registracija.component.ts
@@ -18,7 +18,7 @@ export class RegistracijaComponent implements OnInit {
   provera1()
   {
     var korisnickoIme = (<HTMLInputElement>document.getElementById("korisnickoIme")).value;
-    var regexp1 = new RegExp("^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$");
+    var regexp1 = new RegExp("^(?=.{1,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$");
     var test1 = regexp1.test(korisnickoIme);
     if(!test1 && korisnickoIme)
     {
@@ -37,7 +37,7 @@ export class RegistracijaComponent implements OnInit {
   provera2()
   {
     var email = (<HTMLInputElement>document.getElementById("email")).value;
-    var regexp2 = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
+    var regexp2 = new RegExp("^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
     var test2 = regexp2.test(email);
     if(!test2 && email)
     {
@@ -111,19 +111,19 @@ export class RegistracijaComponent implements OnInit {
 
   registracija(){
     var korisnickoIme = (<HTMLInputElement>document.getElementById("korisnickoIme")).value;
-    var regexp1 = new RegExp("^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$");
+    var regexp1 = new RegExp("^(?=.{1,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$");
     var test1 = regexp1.test(korisnickoIme);
     var email = (<HTMLInputElement>document.getElementById("email")).value;
-    var regexp2 = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
+    var regexp2 = new RegExp("^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$");
     var test2 = regexp2.test(email);
     var ime = (<HTMLInputElement>document.getElementById("ime")).value;
     var regexp3 = new RegExp("^[A-Z]{1}[a-z]+[ ]{1}[A-Z]{1}[a-z]+$");
     var test3 = regexp3.test(ime);
     var sifra = (<HTMLInputElement>document.getElementById("sifra")).value;
-    var regexp4 = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$");
+    var regexp4 = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,60}$");
     var test4 = regexp4.test(sifra); 
     var sifra2= (<HTMLInputElement>document.getElementById("sifra2")).value;
-    var regexp5 = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$");
+    var regexp5 = new RegExp("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,60}$");
     var test5 = regexp5.test(sifra2); 
 
     var pom = false;
@@ -135,8 +135,20 @@ export class RegistracijaComponent implements OnInit {
       this.http.post('http://localhost:5008/api/Auth/register',{"KorisnickoIme":korisnickoIme,"Ime":ime,"Sifra":sifra,"Email":email},{responseType: 'text'}).subscribe(
         res=>{
           console.log(res);
-          alert("Uspesna registracija");
+          //alert("Uspesna registracija");
           this.router.navigate(['/prijava']);
+        },error =>{
+         // console.log(error);
+          if(error.error === "1"){
+            var div1 = (<HTMLDivElement>document.getElementById("podaci1")).innerHTML = "*Korisnik vec postoji";
+            var div2 = (<HTMLDivElement>document.getElementById("podaci2")).innerHTML = "*Korisnik sa ovim email-om vec postoji";
+          }
+          if(error.error === "2"){
+            var div1 = (<HTMLDivElement>document.getElementById("podaci1")).innerHTML = "*Korisnik vec postoji";
+          }
+          if(error.error === "3"){
+            var div2 = (<HTMLDivElement>document.getElementById("podaci2")).innerHTML = "*Korisnik sa ovim email-om vec postoji";
+          }
         }
       );
     }
