2972db30f4810ad0dbca0c03eff7affb6a14c5f2
Ivana Dragovic <ivana.dragovic07@gmail.com>
2022-03-17 09:19:44 +0100

    #47 Podesena pozadina stranice 'Novi eksperiment'
    Podesena je pozadina na stranici 'Novi eksperiment'

--- src/frontend/Angular/src/app/meni.service.ts
+++ src/frontend/Angular/src/app/meni.service.ts
@@ -9,11 +9,11 @@ export class MeniService {
 
   sendTabs(){
     return [
-      {"id": false, "tab": "Početna strana", "style":"color:white", "link":"/pocetna-strana"},
-      {"id": false, "tab": "Novi eksperiment", "style":"color:white", "link":"/novi-eksperiment"},
-      {"id": false, "tab": "Moji eksperiment", "style":"color:white", "link":"/moji-eksperimenti"},
-      {"id": false, "tab": "Kontakt", "style":"color:white", "link":"/kontakt"},
-      {"id": false, "tab": "O nama", "style":"color:white;border-right:none", "link":"/o-nama"}
+      {"id": false, "tab": "Početna strana", "style":"color:white", "link":"/pocetna-strana", "novieks": false},
+      {"id": false, "tab": "Novi eksperiment", "style":"color:white", "link":"/novi-eksperiment", "novieks": true},
+      {"id": false, "tab": "Moji eksperiment", "style":"color:white", "link":"/moji-eksperimenti", "novieks": false},
+      {"id": false, "tab": "Kontakt", "style":"color:white", "link":"/kontakt", "novieks": false},
+      {"id": false, "tab": "O nama", "style":"color:white;border-right:none", "link":"/o-nama", "novieks": false}
     ]
   }
 
