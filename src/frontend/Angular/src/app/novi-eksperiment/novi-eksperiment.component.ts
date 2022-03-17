88c4c1a388ace44194e81f940f5f154449ec2469
Ivana Dragovic <ivana.dragovic07@gmail.com>
2022-03-17 13:42:19 +0100

    #47 Omoguceno ucitavanje .csv fajla
    Omoguceno je ucitavanje .csv fajla na frontend-u

--- src/frontend/Angular/src/app/novi-eksperiment/novi-eksperiment.component.ts
+++ src/frontend/Angular/src/app/novi-eksperiment/novi-eksperiment.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxCsvParser } from 'ngx-csv-parser';
+import { ViewChild } from '@angular/core';
+import { NgxCSVParserError } from 'ngx-csv-parser';
 
 @Component({
   selector: 'app-novi-eksperiment',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoviEksperimentComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
 
+  csvRecords: any[] = [];
+  header = false;
+
+  constructor(private ngxCsvParser: NgxCsvParser) {
+  }
+
+  @ViewChild('fileImportInput', { static: false }) fileImportInput: any;
+
+  // Your applications input change listener for the CSV File
+  fileChangeListener($event: any): void {
+
+    // Select the files from the event
+    const files = $event.srcElement.files;
+
+    // Parse the file you want to select for the operation along with the configuration
+    this.ngxCsvParser.parse(files[0], { header: this.header, delimiter: ',' })
+      .pipe().subscribe((result: any) => {
+
+        console.log('Result', result);
+        this.csvRecords = result;
+      }, (error: NgxCSVParserError) => {
+        console.log('Error', error);
+      });
+  }
 }
