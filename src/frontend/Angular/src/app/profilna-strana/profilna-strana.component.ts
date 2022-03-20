aaa5c784efa088ab0fa7cb60e024214f9b830d3a
Teodora Jeremic <tekiiicaaa99@gmail.com>
2022-03-20 19:23:46 +0100

    #51 Ucitana lista eksperimenata prijavljenog korisnika na profilnoj strani
    - Takodje dodato da se ispisuje poruka za uneseno pogresno korisnicko ime ili lozinku na strani za prijavu

--- src/frontend/Angular/src/app/profilna-strana/profilna-strana.component.ts
+++ src/frontend/Angular/src/app/profilna-strana/profilna-strana.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profilna-strana',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilnaStranaComponent implements OnInit {
 
-  constructor() { }
+  eksperimenti : any[] = [];
+  json: any;
+
+  constructor(private http: HttpClient, public jwtHelper: JwtHelperService, private router: Router) { }
 
   ngOnInit(): void {
+    this.ucitajPodatke();
+  }
+
+  ucitajPodatke()
+  {
+    /*
+    var dekodiraniToken = this.jwtHelper.decodeToken(this.jwtHelper.tokenGetter());
+    var id = dekodiraniToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"];
+*/
+    this.http.get('http://localhost:5008/api/Eksperiment/Eksperimenti').subscribe(
+        res=>{
+          console.log(res);
+          this.json = res;
+          this.eksperimenti = Object.values(this.json);
+        },error =>{
+          console.log(error.error);       
+        }
+      );
   }
 
 }
