3d1607b35611874fd2f242a9f3b4d869d78c49c2
Ivana Dragovic <ivana.dragovic07@gmail.com>
2022-03-26 13:41:29 +0100

    #65 Premestena postojeca logika vezana za ucitavanje, prikaz podataka i pagination
    Premestena je postojeca logika vezana za ucitavanje, prikaz podataka i pagination iz komponente Eksperiment u komponentu Podaci.

--- src/frontend/Angular/src/app/podaci/podaci.component.ts
+++ src/frontend/Angular/src/app/podaci/podaci.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-podaci',
@@ -7,9 +8,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PodaciComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  fileName = '';
+  json: any;
+  page: number = 1;
+  itemsPerPage: any;
+  //totalItems : any;
+  totalItems: number = 0; 
+
+  onFileSelected(event:any) 
+  {
+    const file:File = event.target.files[0];
+
+    if (file) 
+    {
+      this.fileName = file.name;
+
+      const formData = new FormData();
+      formData.append("file", file, this.fileName);
+
+      const upload$ = this.http.post("http://localhost:5008/api/Upload/upload", formData, {responseType: 'text'}).subscribe(
+        res=>{
+          this.loadDefaultItemsPerPage();
+          (<HTMLDivElement>document.getElementById("porukaGreske")).innerHTML = "Uspesno ucitano";
+          (<HTMLSelectElement>document.getElementById("brojRedovaTabele")).style.visibility = "visible";
+          (<HTMLDivElement>document.getElementById("brojRedovaTabelePoruka")).style.visibility = "visible";
+      },error =>{
+        console.log(error.error);	
+        var div = (<HTMLDivElement>document.getElementById("porukaGreske")).innerHTML = "Greška prilikom učitavanja podataka!";
+        (<HTMLSelectElement>document.getElementById("brojRedovaTabele")).style.visibility = "hidden";
+        (<HTMLDivElement>document.getElementById("brojRedovaTabelePoruka")).style.visibility = "hidden";
+      });
+    }
+  }
+
+  loadDefaultItemsPerPage()
+  {      
+    this.http.get("http://localhost:5008/api/Upload/paging/1/10").subscribe(
+       (response: any) => {
+         //console.log(response);
+        console.log(JSON.parse(response.data));
+        this.json =  JSON.parse(response.data);
+         //this.json = response;
+        this.totalItems = response.totalItems;
+    })
+  }
+
+  promeniBrojRedova(value: any)
+  {
+    this.itemsPerPage = parseInt(value);
+    this.http.get("http://localhost:5008/api/Upload/paging/"+this.page+"/" + this.itemsPerPage).subscribe(
+      (response: any) => {
+        this.json =  JSON.parse(response.data);
+        this.totalItems = response.totalItems;
+    })
+  }
+
+  gty(page: any){
+   console.log("---GTY--");
+   this.itemsPerPage = (<HTMLSelectElement>document.getElementById("brojRedovaTabele")).value;
+   console.log(this.itemsPerPage);
+   this.http.get("http://localhost:5008/api/Upload/paging/" + this.page + "/" + this.itemsPerPage).subscribe(
+      (response: any) => {
+        this.json =  JSON.parse(response.data);
+        this.totalItems = response.totalItems;
+    })
+  }
+
+  dajHeadere(): string[]
+  {
+    var headers = Object.keys(this.json[0]);
+    //console.log(Object.values(this.json[0]));
+    return headers;
+  }
+
+  dajRed(i: number)
+  {
+    var redValues = Object.values(this.json[i]);
+    return redValues;
+  }
 }
