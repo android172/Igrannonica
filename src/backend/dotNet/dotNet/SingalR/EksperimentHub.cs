733df893d99b0990f0e8846dffcb708585ecdcc0
Aleksandar Sindjelic <aco.sindjelic@gmail.com>
2022-03-27 21:16:38 +0200

    #73

---
+++ src/backend/dotNet/dotNet/SingalR/EksperimentHub.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+namespace dotNet.SingalR
+{
+    public class EksperimentHub : Hub
+    {
+        public static Dictionary<string,string> users = new Dictionary<string, string>();
+        
+        //token je jwt token korisnika
+        public Task Treniraj(string token,string idmodela)
+        {
+            users[Context.ConnectionId] = token;
+            //users se sastoji iz connectionId i jwt koji ce da posluzi da se uzme MLConnection
+            //Ovde treba pozvati model za treniranje
+            Clients.Clients(Context.ConnectionId).SendAsync("treniranje", "Poceto treniranje");
+            for(int i = 0; i < 10; i++)
+                Clients.Clients(Context.ConnectionId).SendAsync("loss", i);
+            return Clients.Clients(Context.ConnectionId).SendAsync("treniranje", "Treniranje zavrseno");
+        }
+        public void ZaustaviTreniranje()
+        {
+            Console.WriteLine("Treniranje Zaustavljeno");
+            //Potrebno zaustaviti treniranje.
+        }
+        //-------------------------------
+        //U funkciji gde se vraca loss
+        //Clients.Clients(connectionId).SendAsync("loss", value);
+        //-------------------------------
+        public string GetConnectionId() => Context.ConnectionId;
+
+        public override Task OnConnectedAsync()
+        {
+            users.Add(Context.ConnectionId,null);
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            users.Remove(Context.ConnectionId);
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
