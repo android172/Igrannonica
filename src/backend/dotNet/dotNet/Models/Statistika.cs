300cbb547d12cb9df95eda17f68e71df4b095886
Ivana Dragovic <ivana.dragovic07@gmail.com>
2022-03-28 23:21:56 +0200

    #72 Klasa Statistika
    Dodata je nova klasa na backendu za potrebe slanja statistickih pokazatelja na frontend.

---
+++ src/backend/dotNet/dotNet/Models/Statistika.cs
@@ -0,0 +1,16 @@
+﻿namespace dotNet.Models
+{
+    public class Statistika
+    {
+        public Dictionary<string, StatisticsNumerical> statsNum { get; set; }
+        public Dictionary<string, StatisticsNumerical> statsCat { get; set; }
+
+        public Statistika() { }
+
+        public Statistika(Dictionary<string, StatisticsNumerical> statsNum, Dictionary<string, StatisticsNumerical> statsCat)
+        {
+            this.statsNum = statsNum;
+            this.statsCat = statsCat;
+        }
+    }
+}
