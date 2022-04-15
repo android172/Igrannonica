0047ba35319724a7c0cef7fbf613c83c2e26e767
Dejan Belic <android172unity@gmail.com>
2022-04-15 13:02:32 +0200

    Implementiran nacin rukovanja sa greskamo koje nastaju na python serveru. Kod koji rukuje tipovima podataka ucitanog dataset-a je prepravljen kako bi omogucio vecu rigoroznost pri detekciji izuzetaka. Popravljena podela na trening i testni skup. Ovim je omoguceno treniranje bez ucitanog testnog skupa, ali ne i testiranje. Popravljeni bug-ovi vezani za popunjavanje NA vrednosti metodama koje zahtevaju numericke kolone. Popravljeni bug-ovi vezani za popunjavanje NA vrednosti pri cemu se ne update-uje tip kolone. Path do fajlova se sada konstruise pomocu path.join. MSELoss preimenovan u L2Loss.

--- src/backend/dotNet/dotNet/Controllers/MLTestController.cs
+++ src/backend/dotNet/dotNet/Controllers/MLTestController.cs
@@ -133,103 +133,106 @@ namespace dotNet.Controllers {
         [AllowAnonymous]
         [HttpGet]
         public string Test() {
-            if (experiment == null)
-                experiment = new(configuration, "st");  
-            
-            // Load data
-            experiment.LoadDataset(1, "test_data.csv");
+            try
+            {
+                if (experiment == null)
+                    experiment = new(configuration, "st");
 
-            // Get statistics
-            var statistics = experiment.ColumnStatistics();
-            Console.WriteLine(statistics);
+                // Load data
+                experiment.LoadDataset(1, "test_data.csv");
 
-            // Get column types
-            //Console.WriteLine(experiment.GetColumnTypes());
+                // Get statistics
+                var statistics = experiment.ColumnStatistics();
+                Console.WriteLine(statistics);
 
-            // Add row and column
-            //experiment.AddRow(new[] { "1", "1123", "hiThere", "144", "France", "Female", "44", "1", "9",
-            //                          "1", "1", "1", "12412.1", "0"});
-            //int rowCounts = experiment.GetRowCount();
-            //Console.WriteLine(rowCounts);
-            //var column = new string[rowCounts];
-            //for (int i = 0; i < column.Length; i++)
-            //    column[i] = "2";
-            //experiment.AddColumn("IDK", column);
+                // Add row and column
+                experiment.AddRow(new[] { "1", "1123", "hiThere", "144", "France", "Female", "44", "1", "9",
+                                      "1", "1", "1", "12412.1", "0"});
+                int rowCounts = experiment.GetRowCount();
+                Console.WriteLine(rowCounts);
+                //var column = new string[rowCounts];
+                //for (int i = 0; i < column.Length; i++)
+                //    column[i] = "2";
+                //experiment.AddColumn("IDK", column);
 
-            // Normalize rows
-            experiment.ScaleZScore(new int[] { 3, 12 });
-
-
-            // Get rows
-            //var rows = experiment.GetRows(new[] { 0, 1, 2, 3, 5, 6 });
-            //Console.WriteLine(rows);
-
-            // Encode categorical values
-            experiment.OneHotEncoding(new int[] { 4, 5, 13 });
-
-            // Save dataset
-            //try { experiment.SaveDataset(1); }
-            //catch (MLException e) { Console.WriteLine(e.Message); }
-
-            //return "";
-
-            // Replace NA values
-            //experiment.ReplaceZeroWithNA(new int[] { 8 });
-            //experiment.FillNAWithRegression(8, new int[] { 5, 7, 9, 10, 12, 13, 14, 15, 16});
-
-            // Set ANN settings
-            int networkSize = 5;
-
-            // Select inputs, outputs and split data
-            experiment.LoadInputs(new int[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
-            experiment.LoadOutputs(new int[] { 16, 17 });
-            experiment.TrainTestSplit(0.1f);
-
-            int[] hiddentLayers = new int[networkSize];
-            hiddentLayers[0] = 5;
-            hiddentLayers[1] = 7;
-            hiddentLayers[2] = 9;
-            hiddentLayers[3] = 9;
-            hiddentLayers[4] = 7;
-
-            ActivationFunction[] activationFunctions = new ActivationFunction[networkSize];
-            activationFunctions[0] = ActivationFunction.ReLU;
-            activationFunctions[1] = ActivationFunction.ReLU;
-            activationFunctions[2] = ActivationFunction.ReLU;
-            activationFunctions[3] = ActivationFunction.ReLU;
-            activationFunctions[4] = ActivationFunction.ReLU;
-
-            ANNSettings settings = new(
-                aNNType: ProblemType.Regression,
-                learningRate: 0.001f,
-                batchSize:  64,
-                numberOfEpochs: 10,
-                inputSize:  13,
-                outputSize: 2,
-                hiddenLayers: hiddentLayers,
-                activationFunctions: activationFunctions,
-                regularization: RegularizationMethod.L1,
-                regularizationRate: 0.0001f,
-                lossFunction: LossFunction.CrossEntropyLoss,
-                optimizer: Optimizer.Adam,
-                kFoldCV: 0
-                );
-
-            experiment.ApplySettings(settings);
+                // Normalize rows
+                experiment.ScaleZScore(new int[] { 3, 12 });
 
 
-            // Start training
-            experiment.Start();
+                // Get rows
+                var rows = experiment.GetRows(new[] { 0, 1, 2, 3, 5, 6 });
+                Console.WriteLine(rows);
 
-            // Save model / load model
-            Thread.Sleep(10000);
-            try { experiment.LoadEpoch("2"); }
-            catch (MLException e) { Console.WriteLine(e.Message); }
-            experiment.SaveModel("TestModel");
+                // Encode categorical values
+                experiment.OneHotEncoding(new int[] { 4, 5, 13 });
 
-            // Get metrics
-            //var stats = experiment.ComputeMetrics();
-            //Console.WriteLine(stats);
+                // Save dataset
+                //experiment.SaveDataset();
+
+                // Replace NA values
+                experiment.ReplaceZeroWithNA(new int[] { 8 });
+                experiment.FillNAWithRegression(8, new int[] { 5, 7, 9, 10, 12, 13, 14, 15, 16 });
+
+                // Set ANN settings
+                int networkSize = 5;
+
+                // Get column types
+                Console.WriteLine(experiment.GetColumnTypes());
+
+                // Select inputs, outputs and split data
+                experiment.LoadInputs(new int[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 });
+                experiment.LoadOutputs(new int[] { 16, 17 });
+                experiment.TrainTestSplit(0.1f);
+
+                int[] hiddentLayers = new int[networkSize];
+                hiddentLayers[0] = 5;
+                hiddentLayers[1] = 7;
+                hiddentLayers[2] = 9;
+                hiddentLayers[3] = 9;
+                hiddentLayers[4] = 7;
+
+                ActivationFunction[] activationFunctions = new ActivationFunction[networkSize];
+                activationFunctions[0] = ActivationFunction.ReLU;
+                activationFunctions[1] = ActivationFunction.ReLU;
+                activationFunctions[2] = ActivationFunction.ReLU;
+                activationFunctions[3] = ActivationFunction.ReLU;
+                activationFunctions[4] = ActivationFunction.ReLU;
+
+                ANNSettings settings = new(
+                    aNNType: ProblemType.Regression,
+                    learningRate: 0.001f,
+                    batchSize: 64,
+                    numberOfEpochs: 10,
+                    inputSize: 13,
+                    outputSize: 2,
+                    hiddenLayers: hiddentLayers,
+                    activationFunctions: activationFunctions,
+                    regularization: RegularizationMethod.L1,
+                    regularizationRate: 0.0001f,
+                    lossFunction: LossFunction.CrossEntropyLoss,
+                    optimizer: Optimizer.Adam,
+                    kFoldCV: 0
+                    );
+
+                experiment.ApplySettings(settings);
+
+
+                // Start training
+                experiment.Start();
+
+                // Save model / load model
+                //Thread.Sleep(10000);
+                //try { experiment.LoadEpoch("2"); }
+                //catch (MLException e) { Console.WriteLine(e.Message); }
+                //experiment.SaveModel("TestModel");
+
+                // Get metrics
+                var stats = experiment.ComputeMetrics();
+                Console.WriteLine(stats);
+            }
+            catch (MLException e) {
+                Console.WriteLine(e.Message);
+            }
 
             return "done";
         }
--- src/ml/MLData.py
+++ src/ml/MLData.py
@@ -28,27 +28,39 @@ class MLData:
     
     # Load dataset
     def load_from_csv(self, pathOrBuffer):
-        self.dataset = pd.read_csv(pathOrBuffer)
+        dataset = pd.read_csv(pathOrBuffer)
+        self._load_dataset(dataset)
     
     def load_from_json(self, pathOrBuffer):
-        self.dataset = pd.read_json(pathOrBuffer)
+        dataset = pd.read_json(pathOrBuffer)
+        self._load_dataset(dataset)
     
     def load_from_excel(self, pathOrBuffer):
-        self.dataset = pd.read_excel(pathOrBuffer)
+        dataset = pd.read_excel(pathOrBuffer)
+        self._load_dataset(dataset)
+    
+    def _load_dataset(self, dataset):
+        self.dataset = dataset
+        self.train_indices = [i for i in range(self.dataset.shape[0])]
+        self.test_indices = []
+        self._update_column_types()
+    
+    def _update_column_types(self):
+        self.column_types = self.dataset.dtypes
     
     def load_test_from_csv(self, pathOrBuffer):
         dataset_test = pd.read_csv(pathOrBuffer)
-        self.load_test_dataset(dataset_test)
+        self._load_test_dataset(dataset_test)
     
     def load_test_from_json(self, pathOrBuffer):
         dataset_test = pd.read_json(pathOrBuffer)
-        self.load_test_dataset(dataset_test)
+        self._load_test_dataset(dataset_test)
         
     def load_test_from_excel(self, pathOrBuffer):
         dataset_test = pd.read_excel(pathOrBuffer)
-        self.load_test_dataset(dataset_test)
+        self._load_test_dataset(dataset_test)
         
-    def load_test_dataset(self, dataset_test):
+    def _load_test_dataset(self, dataset_test):
         train_length = self.dataset.shape[0]
         self.dataset = pd.concat([self.dataset, dataset_test])
         self.train_indices = [i for i in range(train_length)]
@@ -66,10 +78,20 @@ class MLData:
         
     # Select columns
     def select_input_columns(self, columns):
+        column_types = self.get_column_types()
+        for ci in columns:
+            if column_types[ci] != 'int64' and column_types[ci] != 'float64':
+                return False
         self.input_columns = columns
+        return True
     
     def select_output_columns(self, columns):
+        column_types = self.get_column_types()
+        for ci in columns:
+            if column_types[ci] != 'int64' and column_types[ci] != 'float64':
+                return False
         self.output_columns = columns
+        return True
     
     # Train test splits
     def random_train_test_split(self, ratio):
@@ -81,14 +103,17 @@ class MLData:
         random.shuffle(index_list)
         self.train_indices = index_list[:split_point]
         self.test_indices  = index_list[split_point:]
-        
-    # Column types
-    def initialize_column_types(self):
-        self.column_types = self.dataset.dtypes
 
     # ########### #
     # Data access #
     # ########### #
+    
+    def columns_are_valid(self, columns):
+        length = self.dataset.shape[1]
+        for column in columns:
+            if column < 0 or column >= length:
+                return False
+        return True
     
     def get_train_dataset(self):
         train_data = []
@@ -121,54 +146,82 @@ class MLData:
     
     # Edit dataset
     def update_field(self, row, column, value):
+        if row < 0 or row > self.dataset.shape[0]:
+            return 1
+        if column < 0 or column > self.dataset.shape[1]:
+            return 2
+
+        try:
+            if self.column_types[column] == 'int64':
+                value = int(value)
+            elif self.column_types[column] == 'float64':
+                value = float(value)
+        except:
+            return 3
+        
         self.dataset.iloc[row, column] = value
+        return 0
 
     def add_row(self, new_row, test=False):
         # Convert row values to proper types
         series = pd.Series(new_row, index=self.dataset.columns)
         for i in range(len(new_row)):
-            if self.column_types[i] == 'int64':
-                series[i] = int(series[i])
-            elif self.column_types[i] == 'float64':
-                series[i] = float(series[i])
+            try:
+                if self.column_types[i] == 'int64':
+                    series[i] = int(series[i])
+                elif self.column_types[i] == 'float64':
+                    series[i] = float(series[i])
+            except:
+                return i
         # import values
         self.dataset.loc[self.dataset.shape[0]] = series
         
-        if self.train_indices is not None:
-            if test == False:
-                self.train_indices.append(self.dataset.shape[0])
-            else:
-                self.test_indices.append(self.dataset.shape[0])
+        if test == False:
+            self.train_indices.append(self.dataset.shape[0])
+        else:
+            self.test_indices.append(self.dataset.shape[0])
+        return -1
     
     def update_row(self, row, new_row):
+        if row < 0 or row >= self.dataset.shape[0]:
+            return -2
         # Convert row values to proper types
         series = pd.Series(new_row, index=self.dataset.columns)
         for i in range(len(new_row)):
-            if self.column_types[i] == 'int64':
-                series[i] = int(series[i])
-            elif self.column_types[i] == 'float64':
-                series[i] = float(series[i])
+            try:
+                if self.column_types[i] == 'int64':
+                    series[i] = int(series[i])
+                elif self.column_types[i] == 'float64':
+                    series[i] = float(series[i])
+            except:
+                return i
         # Replace values
         self.dataset.iloc[row] = series
+        return -1
     
     def remove_row(self, row):
+        if row < 0 or row >= self.dataset.shape[0]:
+            return False
         self.dataset.drop(self.dataset.index[row], inplace=True, )
-        if self.train_indices is not None:
-            self.train_indices = [index - 1 if index > row else index for index in self.train_indices if index != row]
-            self.test_indices  = [index - 1 if index > row else index for index in self.test_indices  if index != row]
+        self.train_indices = [index - 1 if index > row else index for index in self.train_indices if index != row]
+        self.test_indices  = [index - 1 if index > row else index for index in self.test_indices  if index != row]
+        return True
             
     def add_column(self, new_column, label):
         series = pd.Series(new_column, name=label)
         self.dataset = self.dataset.join(series)
+        self._update_column_types()
     
     def update_column(self, column, new_column=None, new_label=None):
         if new_label is not None:
             self.dataset.rename(columns={self.dataset.columns[column]:new_label}, inplace=True)
         if new_column is not None:
             self.dataset.iloc[:, column] = new_column
+        self._update_column_types()
         
     def remove_column(self, column):
         self.dataset.drop(self.dataset.columns[column], axis=1, inplace=True)
+        self._update_column_types()
             
     # Handeling NA values
     def replace_value_with_na(self, columns, value):
@@ -180,26 +233,37 @@ class MLData:
     def drop_na_columns(self):
         self.dataset.dropna(axis=1, inplace=True)
 
-    def drop_na_pairwise(self):
-        subset = self.dataset.columns[self.input_columns + self.output_columns]
-        self.dataset.dropna(subset=subset, inplace=True)
-    
-    def drop_na_from_column(self, column):
-        subset = self.dataset.columns[[column]]
+    def drop_na_pairwise(self, columns):
+        subset = self.dataset.columns[columns]
         self.dataset.dropna(subset=subset, inplace=True)
         
     # NA imputing
     def replace_na_with_mean(self, columns):
-        means = self.dataset.iloc[:, columns].mean()
+        try: means = self.dataset.iloc[:, columns].replace(pd.NA, np.nan).astype('float64').mean()
+        except: return False
         self.dataset.fillna(means, inplace=True)
+        self.dataset.iloc[:, columns] = self.dataset.iloc[:, columns].astype('float64')
+        return True
     
     def replace_na_with_median(self, columns):
-        medians = self.dataset.iloc[:, columns].median()
+        try: medians = self.dataset.iloc[:, columns].replace(pd.NA, np.nan).astype('float64').median()
+        except: return False
         self.dataset.fillna(medians, inplace=True)
+        for i in columns:
+            if self.column_types[i] == 'int64':
+                self.dataset.iloc[:, i] = self.dataset.iloc[:, i].astype('int64')
+            elif self.column_types[i] == 'float64':
+                self.dataset.iloc[:, i] = self.dataset.iloc[:, i].astype('float64')
+        return True
     
     def replace_na_with_mode(self, columns):
-        modes = self.dataset.iloc[:, columns].mode().iloc[0]
+        modes = self.dataset.iloc[:, columns].astype(str).replace("<NA>", pd.NA).mode().iloc[0]
         self.dataset.fillna(modes, inplace=True)
+        for i in columns:
+            if self.column_types[i] == 'int64':
+                self.dataset.iloc[:, i] = self.dataset.iloc[:, i].astype('int64')
+            elif self.column_types[i] == 'float64':
+                self.dataset.iloc[:, i] = self.dataset.iloc[:, i].astype('float64')
         
     def replace_na_with_regression(self, column, input_columns):
         if self.dataset.iloc[:, input_columns].isna().any().any() == True:
@@ -208,15 +272,23 @@ class MLData:
         na_rows = self.dataset.iloc[:, column].isna()
         not_na_rows = [not x for x in na_rows]
         inputs = self.dataset[not_na_rows].iloc[:, input_columns]
-        output = self.dataset[not_na_rows].iloc[:, column]
+        try: output = self.dataset[not_na_rows].iloc[:, column].astype('float64')
+        except: return False
         
         regression = LinearRegression().fit(inputs, output)
         predictions = regression.predict(self.dataset[na_rows].iloc[:, input_columns])
         self.dataset.loc[na_rows, self.dataset.columns[column]] = predictions
         
+        if self.column_types[column] == 'int64':
+            self.dataset.iloc[:, column] = self.dataset.iloc[:, column].astype('int64')
+        elif self.column_types[column] == 'float64':
+            self.dataset.iloc[:, column] = self.dataset.iloc[:, column].astype('float64')
+        return True
+        
     # Value encoding
     def label_encode_columns(self, columns):
         self.dataset.iloc[:, columns] = OrdinalEncoder().fit_transform(self.dataset.iloc[:, columns])
+        self._update_column_types()
     
     def one_hot_encode_columns(self, columns):
         encoder = OneHotEncoder()
@@ -225,55 +297,85 @@ class MLData:
                     for i in range(len(columns)) for group in encoder.categories_[i]]
         self.dataset.drop(self.dataset.columns[columns], axis=1, inplace=True)
         self.dataset[new_columns] = result
+        self._update_column_types()
     
     # Normalization
     def maximum_absolute_scaling(self, columns):
         for ci in columns:
             column = self.dataset.iloc[:, ci]
-            self.dataset.iloc[:, ci] = column / column.abs().max()
+            try: self.dataset.iloc[:, ci] = column / column.abs().max()
+            except: return ci
+        self._update_column_types()
+        return -1
     
     def min_max_scaling(self, columns):
         for ci in columns:
             column = self.dataset.iloc[:, ci]
-            self.dataset.iloc[:, ci] = (column - column.min()) / (column.max() - column.min())
+            try: self.dataset.iloc[:, ci] = (column - column.min()) / (column.max() - column.min())
+            except: return ci
+        self._update_column_types()
+        return -1
     
     def z_score_scaling(self, columns):
         for ci in columns:
             column = self.dataset.iloc[:, ci]
-            self.dataset.iloc[:, ci] = (column - column.mean()) / column.std()
+            try: self.dataset.iloc[:, ci] = (column - column.mean()) / column.std()
+            except: return ci
+        self._update_column_types()
+        return -1
         
     # Outlier detection    
     def standard_deviation_outlier_removal(self, columns, treshold):
-        sub_df = self.dataset.iloc[:, columns]
+        try: sub_df = self.dataset.iloc[:, columns].astype('float64')
+        except: return False
         self.dataset = self.dataset[(np.abs(sub_df - sub_df.mean()) < treshold * sub_df.std()).all(axis=1)]
+        return True
     
     def quantile_outlier_removal(self, columns, treshold):
-        sub_df = self.dataset.iloc[:, columns]
+        try: sub_df = self.dataset.iloc[:, columns].astype('float64')
+        except: return False
         self.dataset = self.dataset[((sub_df > sub_df.quantile(treshold)) & (sub_df < sub_df.quantile(1 - treshold))).all(axis=1)]
+        return True
     
     def z_score_outlier_removal(self, columns, treshold):
-        sub_df = self.dataset.iloc[:, columns]
+        try: sub_df = self.dataset.iloc[:, columns].astype('float64')
+        except: return False
         self.dataset = self.dataset[(np.abs(stats.zscore(sub_df)) < treshold).all(axis=1)]
+        return True
         
     def iqr_outlier_removal(self, columns):
-        sub_df = self.dataset.iloc[:, columns]
+        try: sub_df = self.dataset.iloc[:, columns].astype('float64')
+        except: return False
         
         q1 = sub_df.quantile(0.25)
         q3 = sub_df.quantile(0.75)
         iqr = 1.5 * (q3 - q1)
         self.dataset = self.dataset[((sub_df > q1 - iqr) & (sub_df < q3 + iqr)).all(axis=1)]
+        return True
     
     def isolation_forest_outlier_removal(self, columns):
-        rows = IsolationForest().fit_predict(self.dataset.iloc[:, columns])
-        self.dataset = self.dataset[np.where(rows == 1, True, False)]
+        try:
+            rows = IsolationForest().fit_predict(self.dataset.iloc[:, columns])
+            self.dataset = self.dataset[np.where(rows == 1, True, False)]
+            return True
+        except:
+            return False
         
     def one_class_svm_outlier_removal(self, columns):
-        rows = OneClassSVM().fit_predict(self.dataset.iloc[:, columns])
-        self.dataset = self.dataset[np.where(rows == 1, True, False)]
+        try:
+            rows = OneClassSVM().fit_predict(self.dataset.iloc[:, columns])
+            self.dataset = self.dataset[np.where(rows == 1, True, False)]
+            return True
+        except:
+            return False
     
     def local_outlier_factor_outlier_removal(self, columns):
-        rows = LocalOutlierFactor().fit_predict(self.dataset.iloc[:, columns])
-        self.dataset = self.dataset[np.where(rows == 1, True, False)]
+        try:
+            rows = LocalOutlierFactor().fit_predict(self.dataset.iloc[:, columns])
+            self.dataset = self.dataset[np.where(rows == 1, True, False)]
+            return True
+        except:
+            return False
             
     # ######## #
     # Analysis #
@@ -281,9 +383,10 @@ class MLData:
     def get_numerical_statistics(self, columns):
         column_names = self.dataset.columns[columns]
         
-        total_count = self.dataset.shape[0]
-        description = self.dataset[column_names].describe()
+        try: description = self.dataset[column_names].astype('float64').describe()
+        except: return None
         na_counts = self.dataset[column_names].isna().sum()
+        total_count = self.dataset.shape[0]
         valid_counts = total_count - na_counts
         unique_counts = self.dataset[column_names].nunique()
         
@@ -337,3 +440,4 @@ class MLData:
             ).__dict__
     
         return statistics
+    
