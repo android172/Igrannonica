9e848b547670b9ab9f0e4e460a3a353061d46ff9
jovana.markovic <jovana.markovic301@gmail.com>
2022-04-29 21:16:47 +0200

    #108 Izmene na strani modeli

--- src/backend/dotNet/dotNet/Models/ModelDto.cs
+++ src/backend/dotNet/dotNet/Models/ModelDto.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Snap { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
         public string Opis { get; set; }
--- src/frontend/Angular/src/app/modeli/modeli.component.ts
+++ src/frontend/Angular/src/app/modeli/modeli.component.ts
@@ -193,16 +193,40 @@ export class ModeliComponent implements OnInit {
      }
   }
 
+  dajAnn()
+  {
+    if(this.json1["annType"]==1)
+          (<HTMLDivElement>document.getElementById("ann")).innerHTML="Classification";
+        else
+          (<HTMLDivElement>document.getElementById("ann")).innerHTML="Regression";
+  }
+
+  dajOptimizer()
+  {
+    if(this.json1["optimizer"]==0)
+      (<HTMLDivElement>document.getElementById("opt")).innerHTML="SGD";
+    else if(this.json1["optimizer"]==1)
+      (<HTMLDivElement>document.getElementById("opt")).innerHTML="AdaGrad";
+    else if(this.json1["optimizer"]==2)
+      (<HTMLDivElement>document.getElementById("opt")).innerHTML="AdaDelta";
+    else
+    (<HTMLDivElement>document.getElementById("opt")).innerHTML="Adam";
+  }
+
+  dajEpohe()
+  {
+    (<HTMLDivElement>document.getElementById("eph")).innerHTML=this.json1["numberOfEpochs"];
+  }
+
   prikaziPod(id: any)
   {
     this.http.get(url+"/api/Eksperiment/Podesavanja/"+id).subscribe(
       res=>{
         console.log(res);
         this.json1=res;
-        /*if(this.json1["annType"]==1)
-          (<HTMLDivElement>document.getElementById("n")).innerHTML="Classification";
-        else
-          (<HTMLDivElement>document.getElementById("n")).innerHTML="Regression";*/
+        this.dajAnn();
+        this.dajOptimizer();
+        this.dajEpohe();
       },
       error=>{
         console.log(error);
@@ -224,6 +248,7 @@ export class ModeliComponent implements OnInit {
         (<HTMLDivElement>document.getElementById("n")).innerHTML=this.modeli[i].name;
         (<HTMLDivElement>document.getElementById("d")).innerHTML=this.modeli[i].createdDate;
         (<HTMLDivElement>document.getElementById("opis")).innerHTML=this.modeli[i].opis;
+        (<HTMLDivElement>document.getElementById("snap")).innerHTML=this.modeli[i].snap;
       }
     }
     
@@ -235,6 +260,7 @@ export class ModeliComponent implements OnInit {
       (<HTMLDivElement>document.getElementById(this.selektovanModel)).style.background="#C4C4C4";
       (<HTMLDivElement>document.getElementById(this.selektovanModel)).style.color="white";
       (<HTMLDivElement>document.getElementById(this.selektovanModel)).style.transform="scale(1)";
+      (<HTMLDivElement>document.getElementById(this.selektovanModel))
       this.selektovanModel = event.target.id;
       (<HTMLDivElement>document.getElementById(event.target.id)).style.background="linear-gradient(162.06deg,#fa7795 -16.65%,#f0859e 97.46%)";
       (<HTMLDivElement>document.getElementById(event.target.id)).style.transform="scale(1.04)";
