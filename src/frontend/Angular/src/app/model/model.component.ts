247998866dd1f92602d10159829b3966d4d512cd
dragana.andjelkovic <andjelkovic.draganaa@gmail.com>
2022-06-01 08:05:45 +0200

    Dodata komponenta za pracenje i prikaz trenutnog stanja treninga na strani Train network

--- src/frontend/Angular/src/app/model/model.component.ts
+++ src/frontend/Angular/src/app/model/model.component.ts
@@ -195,6 +195,10 @@ export class ModelComponent implements OnInit {
 
   @ViewChild('btnexitoverridemodel') btnexitoverridemodel:any;
 
+  // progress bar
+  public numOfEpochsTotal : number = 0;
+  public currentEpochPercent : number = 0;
+
   constructor(public http: HttpClient,private activatedRoute: ActivatedRoute, private shared: SharedService,public signalR:SignalRService, public modalService : ModalService, private ngbModalService: NgbModal, private router: Router,private service: NotificationsService) { 
     this.activatedRoute.queryParams.subscribe(
       params => {
@@ -259,6 +263,9 @@ export class ModelComponent implements OnInit {
       this.drawCanvas();
 
       this.currentEpoch += 1;
+      // progress bar
+      this.currentEpochPercent = this.currentEpoch;
+      this.currentEpochPercent = this.currentEpochPercent / this.numOfEpochsTotal * 100; 
     });
   }
   sendMessage():void{
@@ -1160,6 +1167,7 @@ export class ModelComponent implements OnInit {
   }
 
   treniraj(){
+    this.currentEpochPercent = 0;
     // Cross validation
     var crossVK;
     if(this.flag == false)
@@ -1185,6 +1193,13 @@ export class ModelComponent implements OnInit {
     // Momentum
     if(this.momentum==true)
       this.optimizationParams[0]=Number((<HTMLInputElement>document.getElementById("momentum")).value);
+    
+    // number of Epochs (Progress bar)
+    var numOfE = Number((<HTMLInputElement>document.getElementById("noe")).value);
+    if(crossVK == 0)
+        this.numOfEpochsTotal = numOfE;
+    else
+        this.numOfEpochsTotal = numOfE * crossVK;
 
     const trainingData = 
     {
